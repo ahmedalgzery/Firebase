4b1cfac00cfb0a54c81881e68776056b0c6186f5
Ahmed Algzery <algzeryahmed14@gmail.com>
2023-06-15 11:20:59 +0300

    Part-9 Check If Use is already Login/Logout

--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -1,3 +1,6 @@
+import 'package:firebase/ui/auth/login_screen.dart';
+import 'package:firebase/utils/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PostScreen extends StatefulWidget {
@@ -8,10 +11,33 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
+  final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Post'),),
+      appBar: AppBar(
+        title: const Text('Post'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              auth.signOut().then((value) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginScreen(),
+                  ),
+                );
+              }).onError((error, stackTrace) {
+                Utils().tostMessage(message: error.toString());
+              });
+            },
+            icon: const Icon(Icons.logout_outlined),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+        ],
+      ),
     );
   }
 }
