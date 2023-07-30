24d3bb26a329eb8c499ca58848273baa4792111c
Ahmed Algzery <algzeryahmed14@gmail.com>
2023-07-30 12:43:13 +0300

    fix overflow

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -1,11 +1,12 @@
+import 'package:firebase/ui/posts/post_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'package:firebase/ui/auth/login_with_phone.dart';
 import 'package:firebase/ui/auth/sinup_screen.dart';
-import 'package:firebase/ui/posts/post_screen.dart';
 import 'package:firebase/utils/utils.dart';
 import 'package:firebase/widgets/round_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -68,87 +69,93 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Form(
-                key: _globalKey,
-                child: Column(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Form(
+                  key: _globalKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      TextFormField(
+                        controller: emailController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter email please ';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Email',
+                            suffixIcon: Icon(Icons.email)),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        controller: passwordController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter password please ';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Password',
+                            suffixIcon: Icon(Icons.lock_outline)),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                RoundButton(
+                  title: 'Login',
+                  loading: loading,
+                  onTap: () {
+                    if (_globalKey.currentState!.validate()) {
+                      login();
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    TextFormField(
-                      controller: emailController,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter email please ';
-                        }
-                        return null;
+                    const Text("Don't have an account ?"),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SingUpScreen(),
+                          ),
+                        );
                       },
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Email',
-                          suffixIcon: Icon(Icons.email)),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      controller: passwordController,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter password please ';
-                        }
-                        return null;
-                      },
-                      keyboardType: TextInputType.text,
-                      decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Password',
-                          suffixIcon: Icon(Icons.lock_outline)),
+                      child: const Text(
+                        'Sing UP',
+                        style: TextStyle(color: Colors.blue),
+                      ),
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              RoundButton(
-                title: 'Login',
-                loading: loading,
-                onTap: () {
-                  if (_globalKey.currentState!.validate()) {
-                    login();
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Don't have an account ?"),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SingUpScreen(),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      'Sing UP',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
+                const SizedBox(
+                  height: 30,
+                ),
               InkWell(
                 onTap: () {
                   Navigator.push(
@@ -170,6 +177,7 @@ class _LoginScreenState extends State<LoginScreen> {
               )
             ],
           ),
+              ),
         ),
       ),
     );
--- lib/ui/auth/sinup_screen.dart
+++ lib/ui/auth/sinup_screen.dart
@@ -55,83 +55,89 @@ class _SingUpScreenState extends State<SingUpScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Form(
-              key: _globalKey,
-              child: Column(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Form(
+                key: _globalKey,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter email please ';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Email',
+                          suffixIcon: Icon(Icons.email)),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      controller: passwordController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter password please ';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Password',
+                          suffixIcon: Icon(Icons.lock_outline)),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              RoundButton(
+                title: 'Sing Up',
+                loading: loading,
+                onTap: () {
+                  singUp();
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextFormField(
-                    controller: emailController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Enter email please ';
-                      }
-                      return null;
+                  const Text("Already have an account ?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                      );
                     },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Email',
-                        suffixIcon: Icon(Icons.email)),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    controller: passwordController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Enter password please ';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.text,
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Password',
-                        suffixIcon: Icon(Icons.lock_outline)),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.blue),
+                    ),
                   ),
                 ],
-              ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            RoundButton(
-              title: 'Sing Up',
-              loading: loading,
-              onTap: () {
-                singUp();
-              },
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Already have an account ?"),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ),
-                    );
-                  },
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ),
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
